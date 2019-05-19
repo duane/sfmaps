2e84c14659d368b3ed42ead12a0f4146ce303f7d
Duane Bailey <bailey.d.r@gmail.com>
2019-05-19 01:06:22 -0700

    Lint everything.

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
-import * as L from 'leaflet';
-import districts from './current-supervisor-districts.json';
+import * as L from 'leaflet'
+import districts from './current-supervisor-districts.json'
 
-const mapEl = document.getElementById('mapid');
+const mapEl = document.getElementById('mapid')
 
-mapEl!.style.cssText = `height: ${window.innerWidth || document.body.clientWidth}px`;
-window.onresize = (event) => {
-  mapEl!.style.cssText = `height: ${window.innerWidth || document.body.clientWidth}px`;
-};
-const map = L.map('mapid');
-var osmUrl='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 12, maxZoom: 20, attribution: osmAttrib});		
+mapEl!.style.cssText = `height: ${window.innerWidth ||
+  document.body.clientWidth}px`
+window.onresize = () => {
+  mapEl!.style.cssText = `height: ${window.innerWidth ||
+    document.body.clientWidth}px`
+}
+const map = L.map('mapid')
+const osmUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const osmAttrib =
+  'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors'
+const osm = new L.TileLayer(osmUrl, {
+  minZoom: 12,
+  maxZoom: 20,
+  attribution: osmAttrib,
+})
 
-map.setView(new L.LatLng(37.773972, -122.431297), 13);
-map.addLayer(osm);
+map.setView(new L.LatLng(37.773972, -122.431297), 13)
+map.addLayer(osm)
 
 L.geoJSON(districts, {
-    onEachFeature: (feature, layer) => layer.bindPopup(feature.properties.supdist)
-  }).
-  addTo(map);
+  onEachFeature: (feature, layer) =>
+    layer.bindPopup(feature.properties.supdist),
+}).addTo(map)
